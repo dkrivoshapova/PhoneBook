69fa53ed364f2eed30244a2ff7b34e682660a1a1
dkrivoshapova <d.krivoshapova@g.nsu.ru >
2022-10-18 18:47:39 +0700

    ready

---
+++ src/PhoneNumberAlreadyExists.java
@@ -0,0 +1,9 @@
+public class PhoneNumberAlreadyExists extends Exception {
+    private String number;
+    public String getPhoneNumber(){return this.number;}
+    public PhoneNumberAlreadyExists(String message,String phoneNumber) {
+        super(message);
+        number = phoneNumber;
+
+    }
+}
