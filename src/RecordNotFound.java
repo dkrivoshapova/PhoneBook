bf594fc15aef7059900b1c5796907515051ad947
dkrivoshapova <73088438+dkrivoshapova@users.noreply.github.com>
2022-11-07 00:27:44 +0700

    Delete src directory

--- src/RecordNotFound.java
+++
@@ -1,8 +0,0 @@
-public class RecordNotFound extends RuntimeException {
-    long iD;
-    public long getId(){return this.iD;}
-    public RecordNotFound(String message, long id) {
-        super(message);
-        this.iD = id;
-    }
-}
